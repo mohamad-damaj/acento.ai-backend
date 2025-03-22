f18c9e5f522129a9cd812ecdfeafb47e4a9b375d
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-03-22 16:36:03 -0400

    fixed trascnription and feedback

--- backend/model/feedback.py
+++ backend/model/feedback.py
@@ -12,15 +12,18 @@ class Gemini:
 
 
     def query_gemini_feedback(self, audio_text, situation, wpm):
-        input_index = f"Role:\nYou are a speech language specialist, you are actively talking to the person, so talk using 'you' but speak like you're talking to a professor as a student, you simple words. You specalize in evaluating the \
-            speech and text based on filler words, words per minute, grammar, and word choice. \n\
+        input_index = f"Role:\nYou are a supporitve speech language specialist, you are actively talking to the person, so refer to the person directly, use simple words, dont be overly formal.\
+            You specalize in evaluating the speech and text based on the below evaluation critera, you will directly be given timestamps\
+            for each of the sentence. \n\
             Evaluation Critera:\n\
                1. Filler Words: Count common filler words (“um,” “like,” “you know,” etc.) and note how they affect clarity.\n\
                2. WPM: Give a brief explanation of how comfortable or rushed this rate might feel given the WPM.\n\
-               3. Grammar: Rate speech grammar on a scale of “Poor/Fair/Good/Excellent,” and briefly explain why citing examples from the given text, \n\
-                ignore puncuation and grammar that is unique to writing.\n\
-               4. Word Choice: State how well the words fit the the given situation (formal, semi-formal, casual). Note if certain words improved or detracted from clarity.\n\
-               5. Overall Recommendation: Keeping in mind the given situation, provide one or two sentences with an overall rating or recommendation.\n\
+               3. Grammar: Rate speech grammar on a scale of Poor/Fair/Good/Excellent, and briefly explain what can be improved by citing examples (dont give any if their grammar is good)\
+                from the given text and give timestamps for the examples, ignore puncuation and grammar that is unique to writing.\n\
+               4. Word Choice: State how well the words fit the the given situation (formal, semi-formal, casual). Note if certain words improved or removed from clarity.\n\
+               5. Comprehensibility: Assess how easy or difficult it is for a reader to grasp that main idea in context with the situation\n\
+               6. Content Structure: Look at how the speaker transitions between points, whether they provide signposts and if the main message is well-supported by evidence or stories.\n\
+               7. Overall Recommendation: Keeping in mind the given situation, provide one or two sentences with an overall rating or recommendation.\n\
             Text to Evaluate: {audio_text}\n\
             Word per Minute: {wpm}\n\
             Situation: {situation}\n\
@@ -29,7 +32,9 @@ class Gemini:
                 2. Estimate WPM and discuss if it's too fast, too slow, or appropriate.\n\
                 3. Assess grammar, including any mistakes, and provide a brief rating.\n\
                 4. Comment on choice of words in context.\n\
-                5. End with an overall recommendation or conclusion in one or two sentences."
+                5. Comment on the comprehensibility of the text, identify any elements that distract from the central message, reference them and their timestamp.\n\
+                6. Comment on the content structure and how well it is\n\
+                7. End with an overall recommendation or conclusion in one or two sentences."
 
         response = self.model.generate_content(input_index)
         return response.text
--- backend/model/transcription.py
+++ backend/model/transcription.py
@@ -1,15 +1,20 @@
 from faster_whisper import WhisperModel
 
-def transcription(audio_path): 
-    model = WhisperModel("large-v3", device="cpu", compute_type="int8")
+class transcriber():
+    def __init__(self):
+        self.model = WhisperModel("medium", device="cpu", compute_type="int8")
 
-    segments, info = model.transcribe(audio_path, beam_size=5, word_timestamps=True)
+    def transcription(self, audio_path): 
 
-    return segments
+        segments, _ = self.model.transcribe(audio_path, beam_size=3)
+
+        return segments
 
 
 if __name__=="__main__":
-    segments = transcription(r"testing\test.mp3")
+    transcriber_model = transcriber()
+
+    segments = transcriber_model.transcription(audio_path=r"testing\test.mp3")
 
     for segment in segments:
         print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
