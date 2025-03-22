d53416d215edf6109221f3bb7caadf7241929d28
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-03-22 15:23:42 -0400

    transcription

--- backend/model/transcription.py
+++ backend/model/transcription.py
@@ -3,11 +3,14 @@ from faster_whisper import WhisperModel
 def transcription(audio_path): 
     model = WhisperModel("large-v3", device="cpu", compute_type="int8")
 
-    segments, info = model.transcribe(audio_path, beam_size=5)
+    segments, info = model.transcribe(audio_path, beam_size=5, word_timestamps=True)
 
-    return (segments, info)
+    return segments
 
 
 if __name__=="__main__":
-    print(transcription(r"testing\test.mp3"))
+    segments = transcription(r"testing\test.mp3")
+
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
 
