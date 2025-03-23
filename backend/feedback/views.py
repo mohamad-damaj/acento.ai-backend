10082de3e059561c569f49697933c17cc281c8c6
BrokenShadow53 <mohamadamaj123@gmail.com>
2025-03-23 10:28:57 -0400

    some changes

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -7,6 +7,7 @@ from ..model.pdf_reader import read_pdf
 import io
 from flask_cors import cross_origin
 import traceback
+import subprocess
 
 
 # Create blueprint for endpoints
@@ -16,13 +17,18 @@ transcribe_model = Transcriber()
 vocal_feature_model = Smile()
 
 @bp.route("/audio", methods=["POST"])
-@cross_origin(origin='localhost', headers=['Content-Type'])
 def audio_feedback():
     if "audio" in request.files:
         audio = request.files["audio"]
     else:
         response = make_response(jsonify("failed to receive file"))
         return response, 400 
+    
+    if "context" in request.files:
+        context = request.files["context"]
+    else:
+        context = None
+
     print(audio.filename)
     audio.save("./file")
     audio.close()
@@ -41,7 +47,7 @@ def audio_feedback():
             print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
         print("audio", audio_text)
         print(wpm(audio_text))
-        feedback = feedback_model.query_gemini_audio_feedback(audio_text, "formal", wpm(audio_text)) # TODO: fix 2nd and 3rd parameters 
+        feedback = feedback_model.query_gemini_audio_feedback(audio_text, "formal", wpm(audio_text), context) # TODO: fix 2nd and 3rd parameters 
         print(feedback)
     except Exception as e:
         print("error:", e)
@@ -49,45 +55,54 @@ def audio_feedback():
         return response, 400
 
     response = make_response(jsonify(clean(feedback)))
+    response.headers["Content-Type"] = "application/json"
     return response, 200
 
 
 @bp.route("/vocal", methods=["POST"])
-@cross_origin(origin='localhost', headers=['Content-Type'])
 def vocal_feedback():
-    if "audio" in request.files:
-        vocal = request.files["audio"]
+    if "vocal" in request.files:
+        vocal = request.files["vocal"]
     else:
         response = make_response(jsonify("failed to receive file"))
-        return response, 400 
+        return response, 401
+
+    if "context" in request.files:
+        context = request.files["context"]
+    else:
+        context = None
 
     print(vocal.filename)
     vocal.save("./file")
     vocal.close()
-
+    subprocess.run(["ffmpeg", "-i", "file", "-acodec", "pcm_u8", "-ar", "22050", "song.wav"])
+    
     try:
-        features, std = vocal_feature_model.feature_extract("./file")
+        features, std = vocal_feature_model.feature_extract("./song.wav")
         print(features)
     except Exception as e:
         print("error:", e)
         response = make_response(jsonify("failed to generate features"))
-        return response, 400
+        return response, 402
     
     try:
-        feedback = feedback_model.query_gemini_vocal_feedback(features, "")
+        feedback = feedback_model.query_gemini_vocal_feedback(features, context)
         print(feedback)
 
     except Exception as e:
         response = make_response(jsonify("failed to generate feedback"))
-        return response, 400
+        
+        return response, 403
         pass
 
     response = make_response(jsonify(clean(feedback)))
+    response = make_response(jsonify(clean(feedback)))
+    response.headers["Content-Type"] = "application/json"
+
     return response, 200
 
 
 @bp.route("/resume", methods=["POST"])
-@cross_origin(origin='localhost', headers=['Content-Type'])
 def resume_feedback():
     if "resume" in request.files:
         resume = request.files["resume"]
@@ -104,7 +119,7 @@ def resume_feedback():
     print(resume.filename)
     resume.save("./file")
     resume.close()
-
+    
     try:
         text = read_pdf("./file")
         
@@ -123,5 +138,7 @@ def resume_feedback():
         pass
 
     response = make_response(jsonify(clean(feedback)))
+    response = make_response(jsonify(clean(feedback)))
+    response.headers["Content-Type"] = "application/json"
     return response, 200
 
